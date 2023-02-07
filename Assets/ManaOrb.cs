e5b48cb00469485dc8548d4bb90a96fdca3a2c8f
Chenhao Wei <chenhao.wei@csu.fullerton.edu>
2023-02-07 00:41:20 -0800

    puzzle

--- Assets/ManaOrb.cs
+++ Assets/ManaOrb.cs
@@ -7,28 +7,41 @@ public class ManaOrb : MonoBehaviour
 
     Rigidbody2D myRigidbody;
     BoxCollider2D myBoxCollider;
+    public PlayerMovement myPlayerMovement;
+    public float moveSpeed;
+
+    Vector3 directionToPlayer;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         myBoxCollider = GetComponent<BoxCollider2D>();
-        StartCoroutine(Spawn());
+       // StartCoroutine(Spawn());
+        myPlayerMovement = FindObjectOfType(typeof(PlayerMovement)) as PlayerMovement;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
-        
-
+        Followplayer();
     }
 
-    IEnumerator Spawn(){
-        myRigidbody.velocity = new Vector2(Random.Range(-0.5f,0.5f),Random.Range(0.1f,0.5f));
-        yield return new WaitForSeconds(5f);
-        myRigidbody.velocity = new Vector2(0,0);
+    void Followplayer(){
 
+        if(!myPlayerMovement.isAlive) {return;}
+
+
+        directionToPlayer = (myPlayerMovement.transform.position - transform.position).normalized;
+        myRigidbody.velocity = new Vector2(directionToPlayer.x, directionToPlayer.y) * moveSpeed;
+    
     }
+
+    // IEnumerator Spawn(){
+    //     myRigidbody.velocity = new Vector2(Random.Range(-0.5f,0.5f),Random.Range(0.1f,0.5f));
+    //     yield return new WaitForSeconds(5f);
+    //     myRigidbody.velocity = new Vector2(0,0);
+
+    // }
  
 }
